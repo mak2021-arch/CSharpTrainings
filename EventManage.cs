f2be6d2e5658194d5100d0acd52348e55d0b5e7d
mak2021-arch <mohammed.asif@successive.tech>
2021-05-06 03:07:06 +0530

    New Encap

--- EventManage.cs
+++ EventManage.cs
@@ -7,39 +7,85 @@ namespace C_
 
         static void Main(String[] args)
         {
+            Manage:
+            Var v = new Var();
             Console.WriteLine("Welcome to MAK Organisers!");
             Console.WriteLine("Please select the type of event");
             Console.WriteLine("1. Birthday \n2. Marriage \n3. Anniversary \n4. Engagement \n5. Reception");
             int eventChoice= Convert.ToInt32(Console.ReadLine());
-            /*Manage obj = new Manage();
-            obj.Guest = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(obj.Guest);*/
+
             switch (eventChoice)
             {
                 case 1:
-                Console.WriteLine("You Havse Selected 'Birthday' for your event");
-                Var v = new Var();
+                Console.WriteLine("You Have Selected 'Birthday' for your event\n");
                 v.MyGuest();
-                Birthday b = new Birthday();
-                b.MyBirthday();
+                v.MyCatering();
                 v.MyDecor();
                 v.MyShift();
                 v.MyDate();
                 v.MyVenue();
+                v.Confirmation();
+                 break;
 
-                break;
+                 case 2:
+                Console.WriteLine("You Have Selected 'Marriage' for your event\n");
+                v.MyGuest();
+                v.MyCatering();
+                v.MyDecor();
+                v.MyShift();
+                v.MyDate();
+                v.MyVenue();
+                v.Confirmation();
+                 break;
+
+                 case 3:
+                Console.WriteLine("You Have Selected 'Anniversary' for your event\n");
+                v.MyGuest();
+                v.MyCatering();
+                v.MyDecor();
+                v.MyShift();
+                v.MyDate();
+                v.MyVenue();
+                v.Confirmation();
+                 break;
+
+                 case 4:
+                Console.WriteLine("You Have Selected 'Engagement' for your event\n");
+                v.MyGuest();
+                v.MyCatering();
+                v.MyDecor();
+                v.MyShift();
+                v.MyDate();
+                v.MyVenue();
+                v.Confirmation();
+                 break;
+
+                  case 5:
+                Console.WriteLine("You Have Selected 'Reception' for your event\n");
+                v.MyGuest();
+                v.MyCatering();
+                v.MyDecor();
+                v.MyShift();
+                v.MyDate();
+                v.MyVenue();
+                v.Confirmation();
+                 break;
+                 
+                   default:
+                   Console.WriteLine("Select a Valid Input. Try Again\n");
+                   goto Manage;
                 
             }
         }
     }
-    // Parent Class to Inherit Values
+    // Class Encapsulation
     class Party
     {
         private int _guest;
         private int _decor;
-        private int _shift;
+        private string _shift;
         private string _date;
-        private int _venue;
+        private string _venue;
 
         public int Guest
         {
@@ -51,68 +97,96 @@ namespace C_
             get { return _decor;}
                 set { _decor = value;}
         }
-        public int Shift
+        public string Shift
         {
           get { return _shift;}
                 set {_shift = value;}
         }
-        public String Date
+        public string PartyDate
         {
             get { return _date;}
                 set {_date = value;}
         }
-        public int Venue
+        public string Venue
         {
             get { return _venue;}
                 set {_venue = value;}
         }
      }
 
-    class Birthday : Party
-    {
-        public void MyBirthday()
-        {
-            Console.WriteLine("Please Select the Catering Option: \n1.'Classic'  --Rs. 200 per plate\n2. 'Regular' --Rs. 400 per plate\n3. 'Exquisite' --Rs. 600 per person ");
-            int cat = Convert.ToInt32(Console.ReadLine());
-            switch (cat)
-            {
-                case 1:
-                
-                break;
-            }
+    
 
-        }
-    }
+   
     // Class Var for I/O operation
      class Var : Party
      {
          public void MyGuest()
-        {
-            Console.WriteLine("Enter the number of Guest \n'Due to Covid Protocols There is a Minimum limit of 40 guests and maximum of 100 Guests'");
-            Var obj = new Var();
+        {   
+            
+            Console.WriteLine("'Enter the number of Guest' \n'Due to Covid Protocols there is a Minimum limit of 40 guests and maximum of 100 Guests'");           
             int noOfGuest = Convert.ToInt32(Console.ReadLine());
-            obj.Guest = noOfGuest;
-         //   if (!Int32.IsNumber)
-          //     Console.WriteLine("Not an integer");
+            if (noOfGuest<40 || noOfGuest>100)
+            {
+             Console.WriteLine("Enter value in the given range");
+                 MyGuest();
+                
+            }
+            else{
+            Guest = noOfGuest;
+            Console.WriteLine("Gathering of:"+Guest);
+            Console.WriteLine();
+                 }
+  
         }
+        public void MyCatering()
+        {
+            Console.WriteLine("Please Select the Catering Option: \n1.'Classic'  --Rs. 200 per plate\n2. 'Regular' --Rs. 400 per plate\n3. 'Exquisite' --Rs. 600 per person\n ");
+            int cateringSelection = Convert.ToInt32(Console.ReadLine());
+            int cc1;
+            switch (cateringSelection)
+            {
+                case 1:
+                Console.WriteLine("You have selected 'Classic' for Catering\n");
+                cc1 = 200*Guest; 
+                Console.WriteLine("The Catering cost for " +Guest+ " people will be: Rs." +cc1 );
+                break;
+
+                case 2:
+                Console.WriteLine("You have selected 'Regular' for Catering\n");
+                 cc1 = 400*Guest; 
+                Console.WriteLine("The Catering cost for " +Guest+ " people will be: Rs." +cc1 );
+                break;
+
+                case 3:
+                Console.WriteLine("You have selected 'Exquiste' for Catering\n");
+                cc1 = 600*Guest; 
+                Console.WriteLine("The Catering cost for " +Guest+ " people will be: Rs." +cc1 );
+                break;
+            }
+
+        }
+       
         public void MyDecor()
         {
             Decoration:
-            Console.WriteLine("Choose the Decoration Type \n1. 'Silver' --Rs. 5000\n2. 'Gold' --Rs. 8000\n3. 'Platinum' --Rs. 10,000/n");
+            Console.WriteLine("\nChoose the Decoration Type \n1. 'Silver' --Rs. 5000\n2. 'Gold' --Rs. 8000\n3. 'Platinum' --Rs. 10,000\n");
             int decorType = Convert.ToInt32(Console.ReadLine());
-            var obj = new Var();
+            Var obj = new Var();
             obj.Decor = decorType;
             switch (decorType)
             {
                 case 1:
-                Console.WriteLine("You have Chosen 'Silver Package' Charges is Rs. 5000/-/n");
+                Console.WriteLine("You have Chosen 'Silver Package' Charges is Rs. 5000/-\n");
                 break;
+
                 case 2:
-                Console.WriteLine("You have Chosen 'Gold Package' Charges is Rs. 8000/-/n");
+                Console.WriteLine("You have Chosen 'Gold Package' Charges is Rs. 8000/-\n");
                 break;
+
                 case 3:
-                Console.WriteLine("You have Chosen 'Platinum Package' Charges is Rs.10,000/-/n");
+                Console.WriteLine("You have Chosen 'Platinum Package' Charges is Rs.10,000/-\n");
                 break;
+
                 default:
                 Console.WriteLine("Enter a Valid Input. Try Again");
                 goto Decoration;
@@ -124,18 +198,32 @@ namespace C_
             Console.WriteLine("Choose the Shift \n1. 'Morning' --Rs. 2000 \n2. 'Evening'  --Rs. 4000\n3. 'Night' --Rs.6000\n");
             int shiftType = Convert.ToInt32(Console.ReadLine());
             var obj = new Var();
-            obj.Shift = shiftType;
+          /*  switch (shiftType)
+            {
+                case 1:
+                Shift = "Morning";
+                break;
+                case 2:
+                Shift = "Evening";
+                break;
+                case 3:
+                Shift = "Night";
+                break;     
+            }*/
             switch (shiftType)
             {
                 case 1:
                 Console.WriteLine("You have Chosen 'Morning Shift' Booking Charges is Rs. 2000/-\n");
                 break;
+
                 case 2:
                 Console.WriteLine("You have Chosen 'Evening Shift' Booking Charges is Rs. 4000/-\n");
                 break;
+
                 case 3:
                 Console.WriteLine("You have Chosen 'Night Shift' Booking Charges is Rs. 6000/-\n");
                 break;
+
                 default:
                 Console.WriteLine("Enter a Valid Input. Try Again");
                 goto Shift;
@@ -145,8 +233,7 @@ namespace C_
         {
             Console.WriteLine("Please Enter the Date:");
             string dateType = Console.ReadLine();
-            var obj = new Var();
-            obj.Date = dateType;
+            PartyDate = dateType;
         }
 
         public void MyVenue()
@@ -154,8 +241,22 @@ namespace C_
             Venue:
             Console.WriteLine("Select the Venue: \n1. Beach Side \n2. Open Garden \n3. Banquet Hall \n4. Paradise Pent House\n");
             int venueType = Convert.ToInt32(Console.ReadLine());
-            var obj = new Var();
-            obj.Venue = venueType;
+            /*switch (venueType)
+            {
+                case 1:
+                Venue = "Beach side";
+                 break;
+                case 2:
+                Venue = "Open Garden";
+                 break;
+                case 3:
+                Venue = "Banquet Hall";
+                 break;
+                case 4:
+                Venue = "Paradise Pent House";
+                 break;
+                }
+            Var obj = new Var();*/
             switch (venueType)
             {
                 case 1:
@@ -174,6 +275,21 @@ namespace C_
                 Console.WriteLine("Enter a Valid Input. Try Again");
                 goto Venue;
             }
+        }
+
+        public void Confirmation(){
+            
+                Console.WriteLine("\nYou have registered your party on " + PartyDate + " for " + Guest + " guests. And the venue is " + Venue + " in " + Shift + ".\n Do you wish to Continue?");
+               string agree = Console.ReadLine();
+
+               if(agree == "y" || agree == "yes" || agree == "Yes" || agree == "YES"){
+                   Console.WriteLine("Your booking has been confirmed!!\n Thank you for choosing us.");
+                   Console.ReadKey();
+               }
+               else if(agree == "n" || agree == "no" || agree == "No" || agree == "NO"){
+                   Console.WriteLine("You cancelled the selection.\n Thank you, visit again..");
+                   Console.ReadKey();
+               }
         }
 
     }
