1ee408ce946854e372051e6f99720c6bb6003e13
mak2021-arch <mohammed.asif@successive.tech>
2021-05-09 02:17:54 +0530

    Link List update

--- listSort.cs
+++ listSort.cs
@@ -1,70 +1,143 @@
 using System;
 using System.Collections.Generic;
-  
-class list {
-    public static void Main(String[] args)
-    {
-        List<int> firstlist = new List<int>();
-        List<string> secondlist = new List<string>();
-        Console.WriteLine("Enter the type of list you want to make\n Type 1 for integer list or type 2 for string list");
-        int n = Convert.ToInt32(Console.ReadLine());
 
-        switch(n)
+namespace C_
+   { 
+class Program
+    {   
+        public static void Main()
         {
-            case 1:
-            Console.WriteLine("Enter the no. of elements you want to insert in the Integer list:");
-            int num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the elements in the list:");
-            for(int i = 0;i<num;i++)
-            {
-                int j = Convert.ToInt32(Console.ReadLine());
-                firstlist.Add(j);
-            }
-            for(int i=0; i<num; i++)
+            Listing:
+            Console.WriteLine("Choose the type of list \n1. Integer; \n2. String;");
+            int choice=Convert.ToInt32(Console.ReadLine());
+            int num2=0;
+            int count =0;
+
+            if (choice==1)
+            {   Number :
+                Console.WriteLine("How many elements in Integer elements?");
+                string num=Console.ReadLine();
+                try
                 {
-                   int count = 1;
-                    for(int j=i+1; j<num-1; j++)
+                 num2=Convert.ToInt32(num);
+                }
+                catch(Exception) {
+                    Console.WriteLine("--------------\nERROR\n--------------\n 'Please Enter Numbers only");
+                    goto Number;
+                }
+                List <int> intlist = new List<int>();
+                List <int> freqlist= new List<int>();
+                Console.WriteLine("Enter the elements:");
+                
+                for (int i=0;i<num2;i++)
+                {
+                    try{
+                    intlist.Add(Convert.ToInt32(Console.ReadLine()));
+                    freqlist.Add(-1);
+                }
+                catch{
+                    Console.WriteLine("Enter Numbers Only");
+                }
+                }
+                  Console.WriteLine("\nOriginal List: ");
+                foreach(int i in intlist)
+                {
+                    Console.Write(" "+i);
+                } 
+                    Console.WriteLine();
+
+                 intlist.Sort();
+                Console.WriteLine("\nSorted List: ");
+                foreach(int i in intlist)
+                {
+                    Console.Write(" "+i);
+                }
+                Console.WriteLine("\n\nFREQUENCY: ");
+
+                 for(int i=0; i<num2; i++)
+                {
+                    count = 1;
+                    for(int j=i+1; j<num2; j++)
                     {
-                        if(firstlist[i]==firstlist[j])
+                        if(intlist[i]==intlist[j])
                         {
-                            count++;
-                            firstlist[j] = 0;
+                          count++;
+                            freqlist[j] = 0;
                         }
                     }
-                if(firstlist[i]!=0)
-                    {
-                        firstlist[i] = count;
-                    }
-            
 
-                // Frequency Printing
-                for(int k=0; k<num; k++)
-                {
-                    if(firstlist[k]!=0)
+                    if(freqlist[i]!=0)
                     {
-                        Console.Write("Frequency of {0} is {1}.\n", firstlist[k]);
+                        freqlist[i] = count;
                     }
-            Console.WriteLine("Count Is: " + firstlist.Count);
+                }
+             for(int i=0; i<num2; i++)
+                {
+                    if(freqlist[i]!=0)
+                    {
+                        Console.Write("Frequency of {0} is {1}.\n", intlist[i], freqlist[i]);
+                    }
+
                 }
             }
-                break;
-
-            case 2:
-            Console.WriteLine("Enter the no. of elements you want to insert in the String list:");
-            int elements = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the elements in the list:");
-            for(int i = 0;i< elements; i++)
+            else if(choice==2)
             {
-                string j = Console.ReadLine();
-                secondlist.Add(j);
+                Console.WriteLine("\nHow many elements in String List?");
+                int num=Convert.ToInt32(Console.ReadLine());
+                List <char> stringlist=new List<char>();
+                List <int> freqlist=new List<int>();
+                Console.WriteLine("\nEnter the elements:");
+                for (int i=0;i<num;i++)
+                {
+                    stringlist.Add(Convert.ToChar(Console.ReadLine()));
+                    freqlist.Add(-1);
+                }
+                  Console.WriteLine("\nOriginal List: ");
+                foreach(char i in stringlist)
+                {
+                    Console.Write(" "+i);
+                } 
+                    Console.WriteLine();
+
+                 stringlist.Sort();
+                Console.WriteLine("\nSorted List: ");
+                foreach(char i in stringlist)
+                {
+                    Console.Write(" "+i);
+                }
+                Console.WriteLine("\n\nFREQUENCY: ");
+
+                 for(int i=0; i<num; i++)
+                {
+                    count = 1;
+                    for(int j=i+1; j<num; j++)
+                    {
+                        if(stringlist[i]==stringlist[j])
+                        {
+                          count++;
+                            freqlist[j] = 0;
+                        }
+                    }
+
+                    if(freqlist[i]!=0)
+                    {
+                        freqlist[i] = count;
+                    }
+                }
+             for(int i=0; i<num; i++)
+                {
+                    if(freqlist[i]!=0)
+                    {
+                        Console.Write("Frequency of {0} is {1}.\n", stringlist[i], freqlist[i]);
+                    }
+
+                }
             }
-            Console.WriteLine("Count Is: " + secondlist.Count);
-            break;
-            
-            default:
-            Console.WriteLine("-------------\n-------------\n 'Select from the given option'");
-            break;
+            else
+            {
+                Console.WriteLine("--------------\nERROR\n--------------\n 'Please input from given choice,'");
+                goto Listing;
+            }
         }
-        
+        }
     }
-}
