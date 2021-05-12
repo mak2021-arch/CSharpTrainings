c4ff814e7056eff97063a690abdecfedeebf6ef4
mak2021-arch <mohammed.asif@successive.tech>
2021-05-12 21:30:50 +0530

    Friday task

--- Friday Task.cs
+++ Friday Task.cs
@@ -7,24 +7,38 @@ namespace C_
 {
     class Car
     {
-        public static void Main()
-        {
-            Var v= new CC();
-                
-            }
-    }
-
-    interface Var
-    {
-     void Color();
+       public string Engine = "V8 Twin Turbo";
+       public string Tyre = "Goodyear Tyre";
+       public string SoundSystem = "BOSE Surround Sound";
 
     }
 
-    class CC : Var
+    interface IColour
     {
-        override Var.Color()
+        string CustomColour();
+    }
+
+    class CarModel : Car , IColour 
+    {
+        public string CustomColour()
         {
-            Console.WriteLine("Color is : Red");
+            Console.WriteLine("Enter Colour name that you want for your car");
+            string col = Console.ReadLine();
+            return col;
         }
     }
-}
+
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            CarModel obj = new CarModel();
+            string a = obj.Engine;
+            string b = obj.Tyre;
+            string c = obj.SoundSystem;
+            string d = obj.CustomColour();
+
+            Console.WriteLine("\nENGINE : " + a + "\nTYRE : " + b + "\nSOUNDSYSTEM : " + c + "\nCOLOUR : " + d); 
+        }
+    }
+} 
